fa703f66ffc45a0beb339b6deb17d1ac26bb90f7
Stiffplay <stephen.rojas@inacapmail.cl>
2026-06-18 15:05:04 -0400

    footer info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,6 +9,11 @@ export default function Footer({
   return (
     <footer className="container" role="contentinfo">
       <p>&copy; Enciclopedia no oficial de League of Legends. Contenido informativo.</p>
+      <p> Nombre alumno: Stephen Nicolás Rojas Parraguez </p>
+      <p> Rut: 21.460.337-3 </p>
+      <p> Nombre Docente: Víctor Vázquez</p>
+      <p> Asignatura: Programación FrontEnd</p>
+      <p> Sección: IEI-N3-P2-C1</p>
 
       <section className="footer-contact" aria-labelledby="contacto-footer-title">
         <h2 id="contacto-footer-title">Formulario de contacto</h2>
